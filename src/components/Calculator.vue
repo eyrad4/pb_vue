ea40f548cec342fdcda3ce3e274469dfb05b5303
eyrad4 <eyrad4@gmail.com>
2018-11-08 10:54:10 +0200

    добавил bootstrap-vue, vue-router, и удалил лишнее

---
+++ src/components/Calculator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">    
+    <p>{{ name }}</p>   
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Hello'
+  }) 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
